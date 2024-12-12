bc9130fba77dea3b0816a84a0cab4361b0ab4f8c
Unikv1 <facundoduenas1@gmail.com>
2024-12-12 19:14:45 -0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 
-  base: '/your-repository-name/',
-  plugins: [react]
+  base: '/quizzical-react/',
+  plugins: [react()]
 
 });
 
